86674e0fcff90f128e9c9fd08d837689a26af55d
kimsejin <kimsejin2118@gmail.com>
2025-02-26 19:55:51 +0900

    fix : password setting ramdom

--- srcs/requirements/backend/srcs/oauth/views.py
+++ srcs/requirements/backend/srcs/oauth/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user_model
 from authentication.views import generate_jwt, jwt_required
 from .utils import get_oauth_token, get_user_info, get_or_create_user
 from friends.views import update_last_activate
+import pyotp
 
 #app -> 42intra login
 @csrf_exempt
@@ -49,8 +50,9 @@ def oauth_callback(request):
 
     email = user_info.get('email')
     username = user_info.get('login')
+    password = pyotp.random_base32()
 
-    user = get_or_create_user(email, username)
+    user = get_or_create_user(email, username, password)
     token = generate_jwt(user, 2)
     
     return JsonResponse({'message': 'User created successfully', 'token': token}, status=201)
