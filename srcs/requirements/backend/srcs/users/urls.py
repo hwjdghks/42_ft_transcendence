d9a204fddba9153a1d923ada9783891f64304854
kimsejin <kimsejin2118@gmail.com>
2025-03-02 19:28:19 +0900

    add : update_password, update_username

--- srcs/requirements/backend/srcs/users/urls.py
+++ srcs/requirements/backend/srcs/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup, signin, signout, withdraw, upload_profile_image, get_profile, update_username, update_password, test_api
+from .views import signup, signin, signout, withdraw, upload_profile_image, get_profile, update_username, update_password
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
@@ -10,5 +10,4 @@ urlpatterns = [
     path('profile/', get_profile, name='profile'),
     path('update/username/', update_username, name='update_username'),
     path('update/password/', update_password, name='update_password'),
-    path('test_api/', test_api, name='test_api'),
 ]
