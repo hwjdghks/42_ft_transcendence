01cc9721917164d4470db5ef417dd7ee4014b72c
jhhwang <5832120@naver.com>
2025-03-10 15:44:57 +0900

    add delete_cookie

--- srcs/requirements/backend/srcs/authentication/urls.py
+++ srcs/requirements/backend/srcs/authentication/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from .views import check_cookie, check_signin_2fa, check_signup_2fa, send_signin_2fa, send_signup_2fa, send_withdraw_2fa
+from .views import (
+    check_cookie,
+    check_signin_2fa,
+    check_signup_2fa,
+    send_signin_2fa,
+    send_signup_2fa,
+    send_withdraw_2fa,
+    remove_cookie,
+)
 
 urlpatterns = [
     path('2fa/signup/request/', send_signup_2fa, name='send_signup_2fa'),
@@ -9,4 +17,5 @@ urlpatterns = [
 	path('2fa/signup/verify/', check_signup_2fa, name='check_signup_2fa'),
 	path('2fa/signin/verify/', check_signin_2fa, name='check_signin_2fa'),
     path('check_cookie/', check_cookie, name='check_cookie'),
+    path('remove_cookie/', remove_cookie, name='remove_cookie'),
 ]
