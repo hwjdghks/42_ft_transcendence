8a236cf8c0cd24e755427da7cb05811a66304c1e
minkyole <minkyole@student.42seuol.kr>
2025-03-11 20:14:40 +0900

    fix : match_result Draw delete

--- srcs/requirements/backend/srcs/matchresult/models.py
+++ srcs/requirements/backend/srcs/matchresult/models.py
@@ -11,7 +11,7 @@ class MatchResult(models.Model):
     user_score = models.IntegerField(default=0, validators=[MinValueValidator(0)]) # 기본값을 0으로 지정 및 최소값을 0으로 지정
     guest_score = models.IntegerField(default=0, validators=[MinValueValidator(0)])# 기본값을 0으로 지정 및 최소값을 0으로 지정
     match_date = models.DateTimeField(auto_now_add=True)
-    game_result = models.CharField(max_length=5, choices=[('win', 'Win'), ('lose', 'Lose'), ('draw', 'Draw')])
+    game_result = models.CharField(max_length=5, choices=[('win', 'Win'), ('lose', 'Lose')])
     session_id = models.UUIDField(primary_key=True, unique=True)  # 자동 생성하지 않고 외부에서 값을 지정합니다.
 
     class Meta:
