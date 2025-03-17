a915d384dc374fff0e261b49aab3523ff38cb5d4
Dongwoo Kim <dongwookim@Dongwooui-MacBookAir.local>
2025-03-17 11:37:40 +0900

    fix: usermodal 수정(2)

--- srcs/requirements/frontend/srcs/scripts/components/modal/usernameModal.js
+++ srcs/requirements/frontend/srcs/scripts/components/modal/usernameModal.js
@@ -17,7 +17,7 @@ export function createUsernameModal() {
     <div class="modal-dialog">
       <div class="modal-content">
          <div class="modal-header">
-           <h5 class="modal-title">${trans[window.curLang].settingModalUsername}</h5>
+           <h5 id="usernameModalLabel" class="modal-title">${trans[window.curLang].settingModalUsername}</h5>
            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
          </div>
          <div class="modal-body">
